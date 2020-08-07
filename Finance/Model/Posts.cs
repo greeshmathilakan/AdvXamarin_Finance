b3491a179be4c88ca9110ec2d4404ce3b0810ad4
Greeshma Thilakan <Greeshma.Thilakan@IRIS.CO.UK>
2020-08-07 13:18:19 +0100

    The first commit

---
+++ Finance/Model/Posts.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Globalization;
+using System.Xml.Serialization;
+
+namespace Finance.Model
+{
+	[XmlRoot(ElementName = "link", Namespace = "http://www.w3.org/2005/Atom")]
+	public class Link
+	{
+		[XmlAttribute(AttributeName = "rel")]
+		public string Rel { get; set; }
+		[XmlAttribute(AttributeName = "href")]
+		public string Href { get; set; }
+	}
+
+	[XmlRoot(ElementName = "enclosure")]
+	public class Enclosure
+	{
+		[XmlAttribute(AttributeName = "url")]
+		public string Url { get; set; }
+		[XmlAttribute(AttributeName = "length")]
+		public string Length { get; set; }
+		[XmlAttribute(AttributeName = "type")]
+		public string Type { get; set; }
+	}
+
+	[XmlRoot(ElementName = "guid")]
+	public class Guid
+	{
+		[XmlAttribute(AttributeName = "isPermaLink")]
+		public string IsPermaLink { get; set; }
+		[XmlText]
+		public string Text { get; set; }
+	}
+
+	[XmlRoot(ElementName = "source")]
+	public class Source
+	{
+		[XmlAttribute(AttributeName = "url")]
+		public string Url { get; set; }
+		[XmlText]
+		public string Text { get; set; }
+	}
+
+	[XmlRoot(ElementName = "item")]
+	public class Item
+	{
+		[XmlElement(ElementName = "title")]
+		public string Title { get; set; }
+		[XmlElement(ElementName = "link")]
+		public string Link2 { get; set; }
+		[XmlElement(ElementName = "description")]
+		public string Description { get; set; }
+		[XmlElement(ElementName = "enclosure")]
+		public Enclosure Enclosure { get; set; }
+		[XmlElement(ElementName = "guid")]
+		public Guid Guid { get; set; }
+		[XmlElement(ElementName = "pubDate")]
+		private string pubDate;
+		public string PubDate 
+		{
+			get { return pubDate; }
+			set
+			{
+				pubDate = value;
+				PublishedDate = DateTime.ParseExact(pubDate, "ddd, dd MMM yyyy HH:mm:ss GMT", CultureInfo.InvariantCulture);
+			}
+		}
+		public DateTime PublishedDate { get; set; }
+		[XmlElement(ElementName = "source")]
+		public Source Source { get; set; }
+	}
+
+	[XmlRoot(ElementName = "channel")]
+	public class Channel
+	{
+		[XmlElement(ElementName = "title")]
+		public string Title { get; set; }
+		[XmlElement(ElementName = "description")]
+		public string Description { get; set; }
+		[XmlElement(ElementName = "link")]
+		public List<string> Link { get; set; }
+		[XmlElement(ElementName = "language")]
+		public string Language { get; set; }
+		[XmlElement(ElementName = "managingEditor")]
+		public string ManagingEditor { get; set; }
+		[XmlElement(ElementName = "webMaster")]
+		public string WebMaster { get; set; }
+		[XmlElement(ElementName = "item")]
+		public List<Item> Item { get; set; }
+	}
+
+	[XmlRoot(ElementName = "rss")]
+	public class Posts
+	{
+		[XmlElement(ElementName = "channel")]
+		public Channel Channel { get; set; }
+		[XmlAttribute(AttributeName = "version")]
+		public string Version { get; set; }
+		[XmlAttribute(AttributeName = "base", Namespace = "http://www.w3.org/XML/1998/namespace")]
+		public string Base { get; set; }
+		[XmlAttribute(AttributeName = "atom", Namespace = "http://www.w3.org/2000/xmlns/")]
+		public string Atom { get; set; }
+		[XmlAttribute(AttributeName = "dc", Namespace = "http://www.w3.org/2000/xmlns/")]
+		public string Dc { get; set; }
+		[XmlAttribute(AttributeName = "itunes", Namespace = "http://www.w3.org/2000/xmlns/")]
+		public string Itunes { get; set; }
+		[XmlAttribute(AttributeName = "media", Namespace = "http://www.w3.org/2000/xmlns/")]
+		public string Media { get; set; }
+	}
+
+}
